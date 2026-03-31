2970cb1058b4be029494ae435b7d561666936279
hankyeongmoo <165135691+hankyeongmoo@users.noreply.github.com>
2026-03-31 14:29:08 +0900

    변수 재정의

--- Assets/03.Scripts/MoveBug.cs
+++ Assets/03.Scripts/MoveBug.cs
@@ -3,23 +3,26 @@ using UnityEngine;
 public class MoveBug : MonoBehaviour
 {
     [Header("버그 속성")]
-    public short bugType;                // 0: 생산자 / 1: 1차 소비자 / 2: 2차 소비자 / 3: 분해자
-    public float moveSpeed;              // 이동 속도
-    public float maxEnergy;              // 에너지 최대치
-    public float energyGainRate;         // 에너지 획득율
-    public float energyConsumptionRate;  // 에너지 소비율
-    public float maxWaste;               // 노폐물 최대치
-    public float wasteProductionRate;    // 노폐물 생산율
-    public float maxDuplicateTime;       // 최대 복제 시간
-    public float minDuplicateTime;       // 최소 복제 시간
-    public float duplicateEnergy;        // 복제 에너지
-    public float duplicateProbability;   // 복제 확률
-    public float deathRateFromInfection; // 감염으로 인한 사망률
+    public short bugType; // 0: 생산자, 1: 1차 소비자, 2: 2차 소비자, 3: 분해자
 
-    [Header("버그 상태")]
-    private float currentEnergy; // 현재 에너지
-    private float currentWaste;  // 현재 노폐물
-    private bool isinfected;     // 감염 여부
+    [Header("물리 및 상태 변수")]
+    public float energy;
+    public int age;
+
+    [Header("유전적 형질 변수")]
+    public float speed;
+    public float detectionRange;
+    public float metabolicRate;
+    public float mutationRate;
+
+    [Header("행동 및 인지 변수")]
+    public string state; // 배회, 추적, 도망, 번식, 휴식
+    public float HungerThreshold;
+    public float ReproductionThreshold;
+
+    [Header("환경 상호작용 변수")]
+    public float temperatureTolerance;
+    public int dietaryPreference; // 0: 생산자, 1: 1차 소비자, 2: 2차 소비자, 3: 분해자
 
     void Start()
     {
